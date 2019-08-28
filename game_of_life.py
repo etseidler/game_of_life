5d2f64c0a2f289e7170b2d0166cbac85634e34ee
Eric Seidler <eric.seidler@gmail.com>
2019-08-28 10:22:59 -0500

    create game of life

---
+++ game_of_life.py
@@ -0,0 +1,72 @@
+import os
+import random
+from collections import namedtuple
+from time import sleep
+
+
+Cell = namedtuple('Cell', 'x y')
+
+
+class Game(object):
+    ALIVE = '\u2588'
+    DEAD = ' '
+
+    def __init__(self, dim=0):
+        self.grid = [list(' ' * dim) for r in range(dim)]
+
+    def __str__(self):
+        return '\n'.join(''.join(row) for row in self.grid)
+
+    @staticmethod
+    def new_state(state, num_live_neighbors):
+        alive_with_two_live_neighbors = state == Game.ALIVE and \
+            num_live_neighbors == 2
+        return Game.ALIVE if alive_with_two_live_neighbors or \
+            num_live_neighbors == 3 else Game.DEAD
+
+    def mark_alive(self, cell):
+        self.grid[cell.x][cell.y] = Game.ALIVE
+
+    def mark_cell(self, cell, state):
+        self.grid[cell.x][cell.y] = state
+
+    def get_cell(self, cell):
+        return self.grid[cell.x][cell.y]
+
+    def tick(self):
+        updated = [row[:] for row in self.grid]
+        for i, row in enumerate(self.grid):
+            for j, _ in enumerate(row):
+                num_live_neighbors = self._count_live_neighbors(i, j)
+                updated[i][j] = self.new_state(
+                    self.get_cell(Cell(i, j)), num_live_neighbors)
+        self.grid = updated
+
+    def random_seed(self):
+        for i, row in enumerate(self.grid):
+            for j, _ in enumerate(row):
+                self.grid[i][j] = random.choice([Game.ALIVE, Game.DEAD])
+
+    def _count_live_neighbors(self, i, j):
+        x_last = len(self.grid) - 1
+        y_last = len(self.grid[0]) - 1
+        count = 0
+        for x in [i-1, i, i+1]:
+            for y in [j-1, j, j+1]:
+                out_of_bounds = x < 0 or y < 0 or x > x_last or y > y_last
+                is_self = (x == i and y == j)
+                if out_of_bounds or is_self:
+                    continue
+                if self.grid[x][y] == Game.ALIVE:
+                    count += 1
+        return count
+
+
+if __name__ == '__main__':
+    game = Game(35)
+    game.random_seed()
+    for _ in range(10000):
+        os.system('clear')
+        print(str(game))
+        game.tick()
+        sleep(0.1)
---
+++ game_of_life_test.py
@@ -0,0 +1,140 @@
+import pytest
+from game_of_life import Game, Cell
+
+
+neighbors_for_upper_left_cell = [
+    Cell(0, 1), Cell(1, 0), Cell(1, 1)
+]
+neighbors_for_upper_center_cell = [
+    Cell(0, 0), Cell(1, 0), Cell(1, 1), Cell(1, 2), Cell(0, 2)
+]
+neighbors_for_middle_cell = [
+    Cell(0, 0), Cell(0, 1), Cell(0, 2),
+    Cell(1, 0), Cell(1, 2),
+    Cell(2, 0), Cell(2, 1), Cell(2, 2)
+]
+
+
+def test_new_instance_creates_empty_grid():
+    game = Game()
+
+    assert len(game.grid) == 0
+
+
+@pytest.mark.parametrize("size", [1, 2, 4, 8, 16, 32, 64])
+def test_grid_size(size):
+    game = Game(size)
+    assert len(game.grid) == size
+    assert all(len(row) == size for row in game.grid)
+
+
+def test_a_cell_is_defaulted_to_dead():
+    game = Game(1)
+
+    assert len(game.grid) == 1
+    assert str(game) == Game.DEAD
+
+
+def test_can_mark_a_cell_as_alive():
+    game = Game(3)
+    cell_1_1 = Cell(1, 1)
+
+    game.mark_alive(cell_1_1)
+
+    assert game.get_cell(cell_1_1) == Game.ALIVE
+
+
+@pytest.mark.parametrize("initial_state,live_neighbors,new_state", [
+    (Game.ALIVE, 0, Game.DEAD),
+    (Game.ALIVE, 1, Game.DEAD),
+    (Game.ALIVE, 2, Game.ALIVE),
+    (Game.ALIVE, 3, Game.ALIVE),
+    (Game.ALIVE, 4, Game.DEAD),
+    (Game.ALIVE, 5, Game.DEAD),
+    (Game.ALIVE, 6, Game.DEAD),
+    (Game.ALIVE, 7, Game.DEAD),
+    (Game.ALIVE, 8, Game.DEAD),
+    (Game.DEAD, 0, Game.DEAD),
+    (Game.DEAD, 1, Game.DEAD),
+    (Game.DEAD, 2, Game.DEAD),
+    (Game.DEAD, 3, Game.ALIVE),
+    (Game.DEAD, 4, Game.DEAD),
+    (Game.DEAD, 5, Game.DEAD),
+    (Game.DEAD, 6, Game.DEAD),
+    (Game.DEAD, 7, Game.DEAD),
+    (Game.DEAD, 8, Game.DEAD)
+])
+def test_new_state_for_live_cells(initial_state, live_neighbors, new_state):
+    assert Game.new_state(initial_state, live_neighbors) == new_state
+
+
+@pytest.mark.parametrize("initial_state,live_neighbors,new_state", [
+    (Game.ALIVE, neighbors_for_middle_cell[0:1], Game.DEAD),
+    (Game.ALIVE, neighbors_for_middle_cell[0:2], Game.ALIVE),
+    (Game.ALIVE, neighbors_for_middle_cell[0:3], Game.ALIVE),
+    (Game.ALIVE, neighbors_for_middle_cell[0:4], Game.DEAD),
+    (Game.ALIVE, neighbors_for_middle_cell[0:5], Game.DEAD),
+    (Game.ALIVE, neighbors_for_middle_cell[0:6], Game.DEAD),
+    (Game.ALIVE, neighbors_for_middle_cell[0:7], Game.DEAD),
+    (Game.ALIVE, neighbors_for_middle_cell, Game.DEAD),
+    (Game.DEAD, neighbors_for_middle_cell[0:1], Game.DEAD),
+    (Game.DEAD, neighbors_for_middle_cell[0:2], Game.DEAD),
+    (Game.DEAD, neighbors_for_middle_cell[0:3], Game.ALIVE),
+    (Game.DEAD, neighbors_for_middle_cell[0:4], Game.DEAD),
+    (Game.DEAD, neighbors_for_middle_cell[0:5], Game.DEAD),
+    (Game.DEAD, neighbors_for_middle_cell[0:6], Game.DEAD),
+    (Game.DEAD, neighbors_for_middle_cell[0:7], Game.DEAD),
+    (Game.DEAD, neighbors_for_middle_cell, Game.DEAD)
+])
+def test_middle_cell_with_neighbors(initial_state, live_neighbors, new_state):
+    game = Game(3)
+    middle_cell = Cell(1, 1)
+    game.mark_cell(middle_cell, initial_state)
+    for cell in live_neighbors:
+        game.mark_alive(cell)
+
+    game.tick()
+
+    assert game.get_cell(middle_cell) == new_state
+
+
+@pytest.mark.parametrize("initial_state,live_neighbors,new_state", [
+    (Game.ALIVE, [], Game.DEAD),
+    (Game.ALIVE, neighbors_for_upper_left_cell[0:1], Game.DEAD),
+    (Game.ALIVE, neighbors_for_upper_left_cell[0:2], Game.ALIVE),
+    (Game.ALIVE, neighbors_for_upper_left_cell, Game.ALIVE),
+    (Game.DEAD, [], Game.DEAD),
+    (Game.DEAD, neighbors_for_upper_left_cell[0:1], Game.DEAD),
+    (Game.DEAD, neighbors_for_upper_left_cell[0:2], Game.DEAD),
+    (Game.DEAD, neighbors_for_upper_left_cell, Game.ALIVE),
+])
+def test_UL_cell_with_neighbors(initial_state, live_neighbors, new_state):
+    game = Game(3)
+    upper_left_cell = Cell(0, 0)
+    game.mark_cell(upper_left_cell, initial_state)
+    for cell in live_neighbors:
+        game.mark_alive(cell)
+
+    game.tick()
+
+    assert game.get_cell(upper_left_cell) == new_state
+
+
+@pytest.mark.parametrize("initial_state,live_neighbors,new_state", [
+    (Game.ALIVE, [], Game.DEAD),
+    (Game.ALIVE, neighbors_for_upper_center_cell[0:1], Game.DEAD),
+    (Game.ALIVE, neighbors_for_upper_center_cell[0:2], Game.ALIVE),
+    (Game.ALIVE, neighbors_for_upper_center_cell[0:3], Game.ALIVE),
+    (Game.ALIVE, neighbors_for_upper_center_cell[0:4], Game.DEAD),
+    (Game.ALIVE, neighbors_for_upper_center_cell, Game.DEAD)
+])
+def test_UC_cell_with_neighbors(initial_state, live_neighbors, new_state):
+    game = Game(3)
+    upper_center_cell = Cell(0, 1)
+    game.mark_cell(upper_center_cell, initial_state)
+    for cell in live_neighbors:
+        game.mark_alive(cell)
+
+    game.tick()
+
+    assert game.get_cell(upper_center_cell) == new_state
