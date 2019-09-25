36e0e960e7fa52ca3b7bcfbadfdf9061c0f0f723
Eric Seidler <eric.seidler@gmail.com>
2019-09-25 15:51:42 -0500

    add empty game_of_life.py file

---
+++ game_of_life.py
@@ -0,0 +1,16 @@
+import os
+import random
+from collections import namedtuple
+from time import sleep
+
+
+Cell = namedtuple('Cell', 'x y')
+
+
+class Game(object):
+    ALIVE = '\u2588'
+    DEAD = ' '
+
+
+if __name__ == '__main__':
+    pass
